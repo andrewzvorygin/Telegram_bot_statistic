11113759598933e4c954a6fc117998745362a695
Андрей <andrewzvorygin@yandex.ru>
2021-07-07 12:55:46 +0500

    инициализация проекта

---
+++ app.py
@@ -0,0 +1,17 @@
+from loader import bot
+from config import admin_id
+
+
+async def on_shutdown(dp):
+    await bot.close()
+
+
+async def on_startup(dp):
+    await bot.send_message(chat_id=admin_id, text='Бот запущен')
+
+
+if __name__ == '__main__':
+    from aiogram import executor
+    from handlers import dp
+
+    executor.start_polling(dp, on_shutdown=on_shutdown, on_startup=on_startup)
