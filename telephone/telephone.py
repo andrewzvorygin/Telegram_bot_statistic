7663fd48ef933685cd6410c10505ff69f0211891
Андрей <andrewzvorygin@yandex.ru>
2021-07-15 18:39:07 +0500

    Получил метод сухраба

--- telephone/telephone.py
+++ telephone/telephone.py
@@ -1,33 +1,59 @@
-import configparser
-import json
+import aiosqlite
+import asyncio
+
+
+async def create_db(chat):
+    async with aiosqlite.connect("user_online.db") as db:
+        await db.execute(f"""CREATE TABLE IF NOT EXISTS {chat} (
+                        date_time text NOT NULL, 
+                        count_user_online int NOT NULL, 
+                        count_user int NOT NULL,
+                        percentage_of_users REAL NOT NULL);
+                   """)
+        await db.commit()
+
+
+async def insert_values_db(chat, date_time, count_user_online, count_user, percentage_of_users):
+    args = (date_time, count_user_online, count_user, percentage_of_users)
+    async with aiosqlite.connect("user_online.db") as db:
+        sqlite_insert = f"""INSERT INTO {chat} (date_time, count_user_online, count_user, percentage_of_users) VALUES (?, ?, ?, ?);"""
+        await db.execute(sqlite_insert, args)
+        await db.commit()
+
+
+async def read_values_db(chat):
+    async with aiosqlite.connect('user_online.db') as db:
+        cursor = await db.execute(f'SELECT * FROM {chat}')
+        rows = await cursor.fetchall()
+        await cursor.close()
+    return rows
+
+
+async def main():
+    chat = 'test'
+    await create_db(chat)
+    await insert_values_db(chat, '2021-07-14 18:00:00:000', 4, 10, 0.4)
+    await insert_values_db(chat, '2021-07-14 19:00:00:000', 6, 10, 0.6)
+    answer = await  read_values_db(chat)
+    print(answer)
+
+asyncio.run(main())
+
+
 import datetime
 from bot.config import api_id, api_hash, username
 from telethon.sync import TelegramClient
 from telethon import types
-
-# для корректного переноса времени сообщений в json
 from datetime import date, datetime
-
-# классы для работы с каналами
 from telethon.tl.functions.channels import GetParticipantsRequest
 from telethon.tl.types import ChannelParticipantsSearch
 
-# класс для работы с сообщениями
-from telethon.tl.functions.messages import GetHistoryRequest
-
-# Считываем учетные данные
-config = configparser.ConfigParser()
-config.read("config.ini")
-
-# Присваиваем значения внутренним переменным
 
 client = TelegramClient(username, api_id, api_hash)
-
 client.start()
 
 
 async def dump_all_participants(channel):
-    """Записывает json-файл с информацией о всех участниках канала/чата"""
     offset_user = 0
     limit_user = 100
     users_online = 0
